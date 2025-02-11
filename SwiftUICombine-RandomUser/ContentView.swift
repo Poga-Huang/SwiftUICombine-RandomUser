5ff8205c687798a907977e9eee16f20aec5583d5
poga07 <chris@168money.com.tw>
2025-02-11 14:19:29 +0800

    Initial Commit

---
+++ SwiftUICombine-RandomUser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICombine-RandomUser
+//
+//  Created by 黃柏嘉 on 2025/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICombine-RandomUser/SwiftUICombine_RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombine_RandomUserApp.swift
+//  SwiftUICombine-RandomUser
+//
+//  Created by 黃柏嘉 on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombine_RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
